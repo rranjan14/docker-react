f25d5c8d3f02f40c4db5a3e035e238919c1fa96c
rranjan14 <rranjan18bcs@iiitkottayam.ac.in>
2020-06-30 20:23:05 +0530

    initial commit

---
+++ build/precache-manifest.6278f1aac5dafbd1fb13f965b8f2ebe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1129161b56d309951c05fac0e5a0851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/js/main.23169dfd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
